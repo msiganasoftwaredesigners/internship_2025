54d468c8695708904430a6f53cc0ca77e4f53a58
Medhanit Getaneh <getanehmedhanit@gmail.com>
2025-08-07 11:47:54 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
   content: ['./build/*.html'],
   theme: {
     extend: { 
-       screens: {
-      'ss': { 'max': '412px' }, 
-      'nn': { 'max': '431px' }, 
-      'll': { 'max': '1024px' },
-      'yy': { 'max': '1280px' },
-      } 
     },
   },
   plugins: [],
